7af901d814c9d3d7514b4d1e901c268d5b4dbc6f
Jess Martin <jessmartin@gmail.com>
2020-06-30 14:59:56 -0400

    Add About page - no link yet!

--- docs/todo.md
+++ docs/todo.md
@@ -7,13 +7,14 @@
 - [x] Remove the link for "Hire me"
 - [x] New favicon
 - [x] Fix up the README.md
-
 - [x] Migrate Google Analytics to the new site
 - [x] Investigate the `seo.js` file and see how we can optimize for SEO
+- [x] Experiment with making favicon circular (consistent background color)
+- [ ] Ensure the site looks great on mobile
+- [ ] Articles section and move those locally from Medium
+
 - [ ] Point DNS for jessmart.in to Netlify
 - [ ] GO LIVE!
 
-- [ ] Articles section and move those locally from Medium
 - [ ] Fun logo
 - [ ] Post a signup form for Newsletter
-- [ ] experiment with making favicon circular (consistent background color)
---
+++ src/pages/about.js
@@ -0,0 +1,75 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const AboutPage = () => (
+  <Layout>
+    <SEO title="Jess Martin - About" />
+    <div class="portrait">
+      <Image />
+    </div>
+    <h2>About Jess</h2>
+
+    <p>
+      Jess Martin is a seasoned software developer and product thinker focusing
+      on the intersection of great technology, legitimate business value, and
+      awesome customer experience. Jess has started several companies including{" "}
+      <a href="https://first.io">First</a>, a category-defining intelligent CRM
+      for real estate agents and <a href="http://ranger.io">Ranger</a>, an
+      uptime monitoring service acquired in 2012. He has led teams to design,
+      develop and deploy dozens of products for companies at various stages,
+      from startups to the Fortune 100.
+    </p>
+
+    <p>
+      More details on{" "}
+      <a href="https://www.linkedin.com/in/jessmartin/">LinkedIn</a>.
+    </p>
+
+    <h2>How I Help</h2>
+
+    <p>
+      I specialize in building web and mobile products, focusing on the
+      full-stack. I'm skilled at all phases of product development, with
+      particular experience in navigating the difficult, uncertain beginning
+      phase. I'm well-suited to help build:
+    </p>
+
+    <ul>
+      <li>Prototypes</li>
+      <li>First versions</li>
+      <li>Architecture overhauls</li>
+      <li>Re-writes</li>
+      <li>Dramatic new features</li>
+    </ul>
+
+    <p>
+      If you're not exactly sure what needs to be built, I can help with that,
+      too. I can assist with the planning conversations, facilitate discussions
+      around trade-offs and suggest creative solutions that help more quickly
+      accomplish the project's goals.
+    </p>
+
+    <p>
+      I love collaborating with teams and I'm also comfortable with building all
+      aspects of the project myself.
+    </p>
+
+    <p>
+      While I have very broad experience, I focus on a few technologies that
+      allow me to provide maximum value:
+    </p>
+    <ul>
+      <li>Ruby (Rails, Sinatra, etc)</li>
+      <li>React or JQuery javascript front-ends</li>
+      <li>React Native for mobile apps</li>
+    </ul>
+
+    <p>Contact me at jessmartin at gmail dot com.</p>
+  </Layout>
+)
+
+export default AboutPage
