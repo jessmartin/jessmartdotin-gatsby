c80251522d2f5d21e1c5e26cc719741e0ff753b3
Jess Martin <jessmartin@gmail.com>
2020-07-01 15:30:41 -0400

    Show markdown-formatted articles

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,5 +35,13 @@ module.exports = {
         head: false,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/src/markdown-pages`,
+      },
+    },
+    `gatsby-transformer-remark`,
   ],
 }
